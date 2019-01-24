addc828d2063f8024fe2ffd7a4beafea89a34eea
sdvcrx <memory.silentvoyage@gmail.com>
2019-01-24 15:54:20 +0800

    tests(utils): add base64 testcases

---
+++ utils/base64_test.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBase64Encode(t *testing.T) {
+	assert.Equal(t, Base64Encode("test:test"), "dGVzdDp0ZXN0")
+}
+
+func TestBase64Decode(t *testing.T) {
+	res, err := Base64Decode("dGVzdDp0ZXN0")
+	assert.Nil(t, err)
+	assert.Equal(t, res, "test:test")
+}

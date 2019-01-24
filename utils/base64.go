2f10e0587b4e90a562889828856d1ef84ee9d354
sdvcrx <memory.silentvoyage@gmail.com>
2019-01-24 15:53:43 +0800

    feat(utils): add base64

---
+++ utils/base64.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"encoding/base64"
+)
+
+func Base64Encode(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
+
+func Base64Decode(s string) (string, error) {
+	decoded, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+	return string(decoded), nil
+}

2dc2277d26cded52e02f82b8ab7d3cb74c69dc71
sdvcrx <memory.silentvoyage@gmail.com>
2019-02-07 13:47:18 +0800

    refactor(auth): refine proxy authorization validate

--- auth.go
+++ auth.go
@@ -8,27 +8,57 @@ import (
 	"spx/utils"
 )
 
+type BasicAuth struct {
+	username string
+	password string
+	credentials string
+	credentialsBase64 string
+}
 
-func ProxyAuthenticateHandler(handle http.HandlerFunc, authUser, authPassword string) http.Handler {
+func (auth BasicAuth) IsEmpty() bool {
+	return auth.username == "" && auth.password == ""
+}
+
+func (auth BasicAuth) Validate (authHeader string) bool {
+	if auth.IsEmpty() {
+		return true
+	}
+	credientials := strings.TrimPrefix(authHeader, "Basic ")
+	if credientials == auth.credentials || credientials == auth.credentialsBase64 {
+		return true
+	}
+	return false
+}
+
+func NewBasicAuth(username, password string) BasicAuth {
 	var proxyCredentials string
 	var proxyCredentialsBase64 string
 
-	if authUser != "" && authPassword != "" {
-		proxyCredentials = fmt.Sprintf("%s:%s", authUser, authPassword)
+	if username != "" && password != "" {
+		proxyCredentials = fmt.Sprintf("%s:%s", username, password)
 	}
-	if len(proxyCredentials) > 0 {
+	if proxyCredentials != "" {
 		proxyCredentialsBase64 = utils.Base64Encode(proxyCredentials)
+	}
+	return BasicAuth{
+		username,
+		password,
+		proxyCredentials,
+		proxyCredentialsBase64,
+	}
+}
+
+
+func ProxyAuthenticateHandler(handle http.HandlerFunc, authUser, authPassword string) http.Handler {
+	auth := NewBasicAuth(authUser, authPassword)
+
+	if auth.IsEmpty() {
+		return handle
 	}
 
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-		if len(proxyCredentials) == 0 {
-			handle(w, r)
-			return
-		}
-
 		// Need authorization
-		credientials := strings.TrimPrefix(r.Header.Get("Proxy-Authorization"), "Basic ")
-		if credientials == "" || (credientials != proxyCredentials && credientials != proxyCredentialsBase64){
+		if auth.Validate(r.Header.Get("Proxy-Authorization")) == false {
 			w.Header().Set("Proxy-Authenticate", "Basic realm=\"Password\"")
 			http.Error(w, "", http.StatusProxyAuthRequired)
 			log.Println("Accessing proxy deny, password is wrong or empty")
